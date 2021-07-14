b1e8c7f71b30b09a1b77dea083f6c92e5129fae1
Oleksandr <centroparotjag@gmail.com>
2021-07-14 21:18:10 +0300

    Create new branch and add files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    printf("Hello World\n");
+
+    printf("Привет, мир\n");
+
+
+
+    return a.exec();
+}
