9bc0fd557106dc9d6b73f2de33f2dc8fd2fdeabb
Oleksandr <centroparotjag@gmail.com>
2021-07-18 13:34:45 +0300

    main.cpp replase

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    printf("Hello World\n");
+    int number = 0;
+    int Check_Status = 1;
 
-    printf("Привет, мир\n");
+    //------- Input of number ------
+    printf("Enter a number to check: ");
+    std::cin >> number;
+
+
+
+    // brute force %
+    for (int i = 2; i < number; ++i){
+        if ( number % i == 0 ) {				// Not natural check
+            Check_Status = 0;
+            break;
+        }
+    }
+
+
+    if ( Check_Status && number > 1){
+        printf("%d - Yes: This number is natural prime\n", number);
+    }
+    else {
+        printf("%d - No: This is not a natural prime number.\n", number);
+    }
 
 
 
