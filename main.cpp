9f788b881ab266df7a97506a663b5fb4ffe12c18
Oleksandr <centroparotjag@gmail.com>
2021-07-24 23:45:34 +0300

    Added c file for homework 6

--- main.cpp
+++ main.cpp
@@ -1,95 +1,64 @@
 #include <QCoreApplication>
-//#include <iostream>
+#include <iostream>
 #include <stdio.h>
-
+#include <math.h>
 
 // 1. ----------------------------------------------------
-void dec_to_bin_recursively (int number){
-
-    if (number > 0){
-
-        dec_to_bin_recursively(number / 2);
-
-        printf("%d", number % 2);
+void bubble_sort_2_dim_arr(int* arr, int len) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = 0; j < len - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {  // swap
+                int t =  arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
     }
-    else {}
-
 }
 
-// 2. ----------------------------------------------------
-int iterative_exponentiation (int a, int b){
 
-    int result = 1;
+void printArray(char Free_text [], int* array, const int row, const int col) {
+       std::cout << Free_text << "\t";
 
-    for (int i = 0; i < b; ++i) {
-        result *= a;
-    }
+       for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
 
-    return result;
+                printf("%2d", array[j + (i * row)]);
+                if (j < 2)
+                    printf(",");
+            }
+            printf("\t");
+        }
+
+        printf("\n");
 }
 
 // 3. ----------------------------------------------------
-int recursive_exponentiation(int a, int b){
-
-    if (b > 0){
-        return a * recursive_exponentiation (a , --b);
-    }
-    else {}
-
-    return 1;
+double f(double t)
+{
+    return sqrt(fabs(t)) + 5 * pow(t, 3);
 }
 
 
-// 4. ----------------------------------------------------
-int recursive_exponentiation_even (int num, int exp){
+// https://en.wikipedia.org/wiki/TPK_algorithm
+void TPK_algorithm (void){
 
-    if ( exp % 2 == 0 ){        // even
-        num *= num;
-        exp /= 2;
-        return recursive_exponentiation(num, exp);
+    double a[11] = {0}, y;
+
+    printf("Введите 11 чисел\n");
+    for (int i = 0; i < 11; i++){
+        scanf("%lf", &a[i]);
     }
-    else {
-        exp--;
-        return exp * recursive_exponentiation(num, exp);
+
+
+    for (int i = 10; i >= 0; i--) {
+        y = f(a[i]);
+        if (y > 400)
+            printf("%d TOO LARGE\n", i);
+        else
+            printf("%d %.16g\n", i, y);
     }
 }
-
-
-// 5. ----------------------------------------------------
-//          0 ----X----
-//          |          |
-//          Y          Y
-//          |          |
-//          |          |
-//           ----X---- N
-//
-// Always start at 0.0
-
-
-//            Y  X
-    const int obstacle [8][8] = { {0,0,1,0,0,0,0,0},      // 1 - obstacle; 0 - free
-                                  {0,0,0,1,0,0,0,0},
-                                  {0,0,0,0,1,0,0,0},
-                                  {0,0,0,0,0,0,0,0},
-                                  {0,0,0,1,0,0,1,0},
-                                  {0,0,1,0,0,1,0,1},
-                                  {0,0,0,0,0,0,0,0},
-                                  {0,0,0,0,0,0,0,0} };
-
-int routes(int x, int y) {
-
-
-    if (obstacle [y][x] == 1)       // add my code for obstacle
-        return 0;
-
-    if (x == 0 && y == 0)
-        return 0;
-    else if (x == 0 ^ y == 0)
-        return 1;
-    else
-        return routes(x, y - 1) + routes(x - 1, y);
-}
-
 
 
 
@@ -100,44 +69,21 @@ int main(int argc, char *argv[])
 {
     QCoreApplication q(argc, argv);
 
-//    1.Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.
-    int dec = 142;
-    printf("dec = %d to bin = 0b", dec);
-    dec_to_bin_recursively (dec);
+//  1.  Реализовать пузырьковую сортировку двумерного массива например,
+//      массив 1,9,2 5,7,6 4,3,8 должен на выходе стать 1,2,3 4,5,6 7,8,9
+
+    int arr [3][3]= { {1,9,2}, {5,7,6}, {4,3,8} };
+
+    printArray((char*) "Before sorting:", *arr, 3, 3);
+
+    bubble_sort_2_dim_arr (*arr, 9);
+
+    printArray((char*) "After sorting:", *arr, 3, 3);
+    printf("\n");
 
 
-//    2. Реализовать функцию возведения числа [a] в степень [b]:
-    int a = 12;
-    int b = 5;
-    printf("\n\niterative:   num[a] = %d, exp[b] = %d; result =%d", a, b, iterative_exponentiation (a, b));
-
-
-//    3. Рекурсивно.
-    printf("\nrecursively: num[a] = %d, exp[b] = %d; result =%d", a, b, recursive_exponentiation (a, b));
-
-
-//    4. Рекурсивно, используя свойство чётности степени (то есть, если степень, в которую нужно возвести число, чётная,
-//      основание возводится в квадрат, а показатель делится на два, а если степень нечётная - результат умножается на основание,
-//      а показатель уменьшается на единицу)
-    printf("\n\nrecursively even: num[a] = %d, exp[b] = %d; result =%d", a, b, recursive_exponentiation_even (a, b));
-
-
-
-//    5. Реализовать нахождение количества маршрутов шахматного короля с препятствиями
-//      (где единица - это наличие препятствия, а ноль - свободная для хода клетка)
-     printf("\n");
-
-     const int sizeX = 8;
-     const int sizeY = 8;
-     for (int y = 0; y < sizeY; ++y) {
-         for (int x = 0; x < sizeX; ++x) {
-             printf("%5d", routes(x, y));
-         }
-         printf("\n");
-     }
-
-
-
+//  3.  Реализовать алгоритм Трабба-Прадо-Кнута в коде на языке С
+    TPK_algorithm ();
 
     printf("\n");
     return 0;
